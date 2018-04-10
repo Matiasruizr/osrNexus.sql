0f4f18440645e0f45889d6cf03defadd42fe5112
Matiasruizr <matiasruizr@hotmail.es>
2018-04-10 20:30:46 -0300

    Asesorías, bono colación, bono movilización

--- main.sql
+++ main.sql
@@ -3,13 +3,8 @@ select * from PORC_ASESORIAS;
 set serveroutput on;
 
 
-
-
 select * from REMUNERACION_MENSUAL;
-
-
 SELECT * FROM PROFESIONAL;
-
 
 DECLARE 
   VAR_NRO_PROFESIONALES NUMBER := 0;
@@ -20,6 +15,9 @@ DECLARE
   VAR_BONO_IMAGEN NUMBER := 0;
   VAR_BONO_ASESORIAS NUMBER := 0;
   VAR_NRO_ASESORIAS NUMBER := 0;
+  VAR_PORCENTAJE_ASESORIAS NUMBER := 0;
+  VAR_BONO_COLACION NUMBER := 0;
+  VAR_BONO_MOVILIZACION NUMBER := 0;
 BEGIN
   -- CUENTA CUANTOS PROFESIONALES HAY EN LA TABLA
   SELECT COUNT(IDPROFESIONAL) INTO VAR_NRO_PROFESIONALES FROM PROFESIONAL;
@@ -47,12 +45,11 @@ BEGIN
        DBMS_OUTPUT.PUT(VAR_BONO_IMAGEN||' ');
        
        
-       /*•	El monto de bono asesorías corresponde a un porcentaje (2%, 3%, 5% y 7%)
+       /*â€¢	El monto de bono asesorÃ­as corresponde a un porcentaje (2%, 3%, 5% y 7%)
        del sueldo base del profesional de acuerdo con la cantidad de
-       asesorías prestadas en el mes de cálculo y en base a los tramos existentes en la tabla porc_asesorias:*/
+       asesorÃ­as prestadas en el mes de cÃ¡lculo y en base a los tramos existentes en la tabla porc_asesorias:*/
      --  VAR_BONO_ASESORIAS :=
 
-     
        SELECT COUNT(A.RUTPROF) 
        INTO VAR_NRO_ASESORIAS
        FROM PROFESIONAL P 
@@ -60,8 +57,23 @@ BEGIN
        GROUP BY A.RUTPROF,P.IDPROFESIONAL
        HAVING P.IDPROFESIONAL = I;
        
+       IF VAR_NRO_ASESORIAS > 0  AND VAR_NRO_ASESORIAS <= 10 THEN
+        VAR_PORCENTAJE_ASESORIAS := 2;
+       ELSIF VAR_NRO_ASESORIAS > 10  AND VAR_NRO_ASESORIAS <= 20 THEN
+        VAR_PORCENTAJE_ASESORIAS := 3;
+       ELSE
+        VAR_PORCENTAJE_ASESORIAS := 0;
+       END IF;
+       VAR_BONO_ASESORIAS := ((VAR_PORCENTAJE_ASESORIAS/100)* VAR_SUELDO_BASE);
+       DBMS_OUTPUT.PUT(VAR_BONO_ASESORIAS||' ');
+      
+      
+       VAR_BONO_COLACION := :Bono_Colacion;
+       DBMS_OUTPUT.PUT(VAR_BONO_COLACION||' ');
        
-       
+        VAR_BONO_MOVILIZACION := :Bono_Movilizacion;
+       DBMS_OUTPUT.PUT(VAR_BONO_MOVILIZACION||' ');
+      
        DBMS_OUTPUT.PUT_LINE(' ');
        --INSERT INTO REMUNERACION_MENSUAL VALUES(RUTPROF,ETC,ETC);
   END LOOP;
@@ -69,8 +81,16 @@ BEGIN
 END;
 /
 
- /*•	El monto de bono asesorías corresponde a un porcentaje (2%, 3%, 5% y 7%)
+ /*â€¢	El monto de bono asesorÃ­as corresponde a un porcentaje (2%, 3%, 5% y 7%)
        del sueldo base del profesional de acuerdo con la cantidad de
-       asesorías prestadas en el mes de cálculo y en base a los tramos existentes en la tabla porc_asesorias:*/
+       asesorÃ­as prestadas en el mes de cÃ¡lculo y en base a los tramos existentes en la tabla porc_asesorias:*/
        SELECT * FROM PORC_ASESORIAS;
        
+      
+        SELECT A.RUTPROF,COUNT(A.RUTPROF) 
+       FROM PROFESIONAL P 
+       JOIN ASESORIA A ON P.RUTPROF = A.RUTPROF
+       GROUP BY A.RUTPROF,P.IDPROFESIONAL;
+       HAVING P.IDPROFESIONAL = 1;
+       
+      
